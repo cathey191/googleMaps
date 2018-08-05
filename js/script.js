704ddc04fbd266ef61c2f26f29c090524396fd86
Catherine Clague <845870941@desktop-cnc8ria.acg.ac.nz>
2018-08-06 11:52:24 +1200

    Styling Map

--- js/script.js
+++ js/script.js
@@ -6,8 +6,50 @@ function initMap() {
 			lat: -41.279214,
 			lng: 174.78034
 		},
-		zoom: 15,
-		disableDefaultUI: true
+		zoom: 6,
+		disableDefaultUI: true,
+		mapTypeControl: true,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_BOTTOM
+		},
+		fullscreenControl: true,
+		fullscreenControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_BOTTOM
+		},
+		streetViewControl: true,
+		streetViewControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_BOTTOM
+		},
+		styles: [
+			{
+				featureType: 'water',
+				stylers: [{ color: '#F0F0F0' }]
+			},
+			{
+				featureType: 'road',
+				elementType: 'geometry',
+				stylers: [{ color: '#7CE9E6' }]
+			},
+			{
+				featureType: 'landscape',
+				elementType: 'geometry',
+				stylers: [{ color: '#FFFFFF' }]
+			},
+			{
+				featureType: 'landscape',
+				elementType: 'labels',
+				stylers: [{ visibility: 'off' }]
+			},
+			{
+				featureType: 'poi.park',
+				stylers: [{ visibility: 'off' }]
+			},
+			{
+				featureType: 'administrative',
+				elementType: 'labels.text.fill',
+				stylers: [{ color: '#403042' }]
+			}
+		]
 	};
 
 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);

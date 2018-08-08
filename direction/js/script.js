7d8f652b4ab9cd2ef56e4f75e6d69d83366b8d8f
Catherine Clague <845870941@desktop-cnc8ria.acg.ac.nz>
2018-08-09 09:35:47 +1200

    directions file setup

--- direction/js/script.js
+++ direction/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var map;
 	var markersArray = [];
 	var infoBox;
-	document.querySelector('#submit').addEventListener('click', filter, false);
+	document.querySelector('#findMe').addEventListener('click', findMe, false);
 
 	$.ajax({
 		url: 'config.json',
@@ -41,100 +41,9 @@ $(document).ready(function() {
 			},
 			zoom: 14
 		});
-
-		getMarkers();
 	}
 
-	function getMarkers() {
-		$.ajax({
-			url: 'data/poi.json',
-			dataType: 'json',
-			type: 'GET',
-			success: function(data) {
-				for (var i = 0; i < data.length; i++) {
-					createMarkers(data[i]);
-					listPlaces(data[i]);
-				}
-
-				document.querySelector('#places');
-				addEventListener('click', placeListner, false);
-			},
-			error: function(error) {
-				console.log('Error');
-				console.log(error);
-			}
-		});
-	}
-
-	function createMarkers(data) {
-		var marker = new google.maps.Marker({
-			position: {
-				lat: data.lat,
-				lng: data.long
-			},
-			title: data.place_name,
-			dataID: data.id,
-			map: map
-		});
-
-		markerListner(marker);
-
-		markersArray.push(marker);
-	}
-
-	function listPlaces(data) {
-		var newPlace = '<div class="place" data-id=' + data.id + '>';
-		newPlace += '<h3>' + data.place_name + '</h3>';
-		newPlace += '<p id="type">' + data.place_type + '</p>';
-		newPlace += '<p id="price">Price: ' + checkPrice(data) + '</p>';
-		newPlace += '</div><hr>';
-
-		$('#places').append(newPlace);
-	}
-
-	function checkPrice(data) {
-		if (data.cost === false) {
-			return 'Free';
-		} else {
-			return '$' + data.cost;
-		}
-	}
-
-	function markerListner(marker) {
-		infoBox = new google.maps.InfoWindow();
-		google.maps.event.addListener(marker, 'click', function() {
-			infoBox.setContent('<div><strong>' + marker.title + '</strong></div>');
-			infoBox.open(map, marker);
-			map.panTo(marker.position);
-			map.setZoom(15);
-		});
-	}
-
-	function placeListner(e) {
-		if (typeof e.target.parentElement.dataset.id === 'string') {
-			var id = e.target.parentElement.dataset.id;
-		} else if (typeof e.target.dataset.id === 'string') {
-			var id = e.target.dataset.id;
-		} else {
-			return;
-		}
-
-		for (var i = 0; i < markersArray.length; i++) {
-			if (id == markersArray[i].dataID) {
-				infoBox.setContent(
-					'<div><strong>' + markersArray[i].title + '</strong></div>'
-				);
-				infoBox.open(map, markersArray[i]);
-				map.panTo(markersArray[i].position);
-				map.setZoom(15);
-			}
-		}
-	}
-
-	function filter(e) {
-		event.preventDefault();
-		console.dir(e.target);
-	}
+	function findMe() {}
 
 	// close of doc
 });
